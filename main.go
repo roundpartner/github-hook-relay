3ef9c25fdd9e05ef66327ac3db0286f92f7016d5
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2019-02-11 02:07:08 +0000

    Increment sleep time when messages are not being received

--- main.go
+++ main.go
@@ -9,24 +9,34 @@ import (
 func main() {
 	log.SetOutput(os.Stdout)
 	queue := os.Getenv("AWS_SQS_QUEUE")
+	multiply := time.Duration(1)
 	for {
-		RelayHook(queue)
-		time.Sleep(time.Second)
+		result, err := RelayHook(queue)
+		if err != nil {
+			log.Printf("[Error] %s", err)
+			multiply = multiply * 2
+		}
+		if result {
+			multiply = time.Duration(1)
+		} else if multiply < 32 {
+			multiply = multiply * 2
+		}
+		time.Sleep(time.Second * multiply)
 	}
 }
 
-func RelayHook(queue string) error {
+func RelayHook(queue string) (bool, error) {
 	message, err := ReadHookFromSqs(queue)
 	if err != nil {
-		return err
+		return false, err
 	}
 	if message == nil {
-		return nil
+		return false, nil
 	}
 	err = RelayRequestHook(message)
 	if err != nil {
-		return err
+		return true, err
 	}
 	DeleteMessage(message.QueueUrl, message.ReceiptHandle)
-	return nil
+	return true, nil
 }
