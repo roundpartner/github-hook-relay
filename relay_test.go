6dd6e0a725d60a4ec61d350a4d3a9fee93a99345
Tom Lorentsen <toml@giffgaff.co.uk>
2019-09-02 11:19:13 +0100

    Add a tag hook request

--- relay_test.go
+++ relay_test.go
@@ -27,3 +27,22 @@ func TestRelayRequest(t *testing.T) {
 		t.Fatalf("%s", err.Error())
 	}
 }
+
+func TestRelayTagRequest(t *testing.T) {
+	content, err := ioutil.ReadFile("tests/tag-hook.req")
+	if err != nil {
+		t.Skipf("%s", err.Error())
+	}
+	defer gock.Off()
+	gock.New("http://localhost:8080").
+		Post("/github-webhook").
+		Reply(http.StatusOK)
+
+	os.Setenv("JENKINS_ENDPOINT", "http://localhost:8080/github-webhook/")
+	buffer := bytes.NewBuffer(content)
+
+	err = RelayRequest(buffer, "application/json", "sha1=0000000000000000000000000000000000000000", "push")
+	if err != nil {
+		t.Fatalf("%s", err.Error())
+	}
+}
